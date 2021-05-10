4a50124b67ba9f28ae2de961daf250870441ba51
Praveen Perera <praveen@avencera.com>
2021-05-10 12:48:44 -0400

    Add structopt and setup interactive flag

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,87 @@ use std::{
     thread,
     time::{Duration, Instant},
 };
+use structopt::clap::AppSettings;
 use tui::{backend::CrosstermBackend, Terminal};
 
 pub type Backend = CrosstermBackend<Stdout>;
 pub type Term = Terminal<Backend>;
 
+use structopt::StructOpt;
+
+/// Simplicity of paper backup with security of a multi-signature
+#[derive(StructOpt, Debug)]
+#[structopt(name = "splitmonic", global_settings = &[AppSettings::ColoredHelp, AppSettings::ArgRequiredElseHelp])]
+enum Splitmonic {
+    #[structopt(
+        name = "split",
+        about = "Split you're mnemonic into multiple split phrases"
+    )]
+    Split {
+        #[structopt(short, long, help = "use the interactive TUI")]
+        interactive: bool,
+
+        #[structopt(
+            short,
+            long,
+            help = "your mnemonic",
+            required_unless = "interactive",
+            conflicts_with = "interactive"
+        )]
+        mnemonic: Option<String>,
+    },
+    #[structopt(
+        name = "combine",
+        about = "Combine you're split phrases into your original mnemonic"
+    )]
+    Combine {
+        #[structopt(short, long, help = "use the interactive TUI")]
+        interactive: bool,
+
+        #[structopt(
+            short,
+            long,
+            help = "3 of your 5 split phrases",
+            required_unless = "interactive",
+            conflicts_with = "interactive",
+            use_delimiter = true,
+            require_delimiter = true,
+            min_values = 3
+        )]
+        split_phrases: Vec<String>,
+    },
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let opt = Splitmonic::from_args();
+
+    match opt {
+        Splitmonic::Split {
+            interactive: true, ..
+        } => setup_split_tui(),
+
+        Splitmonic::Split {
+            mnemonic: Some(mnemonic),
+            ..
+        } => Ok(()),
+
+        Splitmonic::Combine {
+            interactive: true, ..
+        } => Ok(()),
+
+        Splitmonic::Combine {
+            split_phrases: phrases,
+            ..
+        } => Ok(()),
+
+        // any other combinations are impossible
+        _ => Err(eyre::eyre!("unreachable")),
+    }
+}
+
+fn setup_split_tui() -> Result<()> {
     let mut stdout = io::stdout();
 
     terminal::enable_raw_mode()?;
